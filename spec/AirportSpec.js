bdcb100e8bf113ef0af04672034c201b5714c1ad
bensheridanedwards <bensheridanedwards@gmail.com>
2019-12-10 10:44:34 +0000

    Writing Plane unit tests

---
+++ spec/AirportSpec.js
@@ -0,0 +1,11 @@
+describe("Airport", function() {
+  var airport;
+  var plane; 
+  var weather;
+
+  beforeEach(function() {
+    airport = new Airport();
+    plane = new Plane();
+    weather = new Weather();
+  });
+});
