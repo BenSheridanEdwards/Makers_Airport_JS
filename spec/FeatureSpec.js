4022c54c50f8214eb88e31ccecea321d401b6800
bensheridanedwards <bensheridanedwards@gmail.com>
2019-12-10 12:18:37 +0000

    Adding feature test for stormy weather

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -19,4 +19,11 @@ describe('Feature Test:', function(){
     plane.takeoff();
     expect(airport.planes()).not.toContain(plane);
   });
+
+  it('prevents take off when the weather is stormy', function(){
+    plane.land(airport)
+    spyOn('airport','isStormy').and.returnValue(true);
+    expect(function(){ plane.takeoff();}).toThrowError('cannot takeoff during a storm');
+    expect(airport.planes()).toContain(plane);
+  });
 });
